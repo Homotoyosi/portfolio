fe30d2f4415007208d021e1ceb566828cd35332d
Homotoyosi <homotoyosi99@gmail.com>
2024-11-02 21:28:10 +0100

    commit

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -5,9 +5,13 @@ const Hero =()=>{
         <div>
             <div className="herocontainer">
                 <div className="textbox">
-                    <h1>Hi, my name is Raheem Muiz.</h1>
+                    <h1><span>Hi, my name is </span>Raheem Muiz.</h1>
                     <p>A front-end developer with a strong passion for creating accessible, user-friendly websites that provide an exceptional experience for all users.</p>
-
+                    <div>
+                        <button>Contact Me</button>
+                        <button> <img src="" alt="" /></button>
+                        <button> <img src="" alt="" /></button>
+                    </div>
                 </div>
                 <div className="imagebox">
                     <img src={image} alt="" />
