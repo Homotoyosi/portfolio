79e905ee04c72c03f7e65c3b1816fbc39cb65137
Homotoyosi <homotoyosi99@gmail.com>
2024-11-05 10:59:20 +0100

    commit now

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -3,6 +3,11 @@ import image from "/image.jpg"
 import linkedin from "/linkedin-logo.png"
 import githubimg from "/github.png"
 const Hero =()=>{
+    const scrollToContact = () => {
+        const contactSection = document.getElementById('contact-section');
+        contactSection.scrollIntoView({ behavior: 'smooth' });
+    };
+    
     return(
         <div>
             <div className="herocontainer">
@@ -10,7 +15,7 @@ const Hero =()=>{
                     <h1><span>Hi, my name is </span>Raheem Muiz</h1>
                     <p>Software developer and product designer focused on building accessible, user-friendly websites and mobile app for an exceptional user experience.</p>
                     <div className="buttonbox">
-                        <button id="btn1">Contact Me</button>
+                        <button id="btn1" onClick={scrollToContact} >Contact Me</button>
                         <a href="https://www.linkedin.com/in/raheemmuiz/" target="_blank" rel="noopener noreferrer"> <button id="btn2"> <img src={linkedin} alt="" /></button></a>
                         <a href="https://github.com/Homotoyosi" target="_blank" rel="noopener noreferrer"> <button id="btn3"> <img src={githubimg} alt="" /></button></a>
                     </div>
--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -8,14 +8,14 @@ const Contact =()=>{
                 <div>
                 <h1>Contact Me</h1>
                 <h4>Connect with me <a href="mailto:raheemmuiz613@gmail.com" className="email-link">raheemmuiz613@gmail.com</a></h4>
-                <p>For more info, here is my resume <a href="your-resume-link.pdf" target="_blank" className="resume">Resume</a></p>
+                <p>For more info, here is my resume <a href="https://drive.google.com/file/d/1VaCIwlFvQqDCtmcuXyB0FmgVC-T_NhYq/view?usp=sharing" target="_blank" className="resume">Resume</a></p>
                 </div>
                 <div className="btn">
                 <a href="https://www.linkedin.com/in/raheemmuiz/" target="_blank" rel="noopener noreferrer"><button id="btn2"> <img src={linkedin} alt="" /></button></a>
                 <a href="https://github.com/Homotoyosi" target="_blank" rel="noopener noreferrer"><button id="btn3"> <img src={githubimg} alt="" /></button></a>
                 </div>
             </div>
-            <div className="formbox">
+            <div id="contact-section" className="formbox">
                 <form action="" className="form">
                     <div>
                           <label >Name</label>
