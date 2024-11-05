046f7483b0c8e0ec44c6be7a558df8007fa3e2ff
Homotoyosi <homotoyosi99@gmail.com>
2024-11-05 03:17:57 +0100

    now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./page/Home/Home"
 import Header from "./static/Header/Header"
+import Footer from "./static/Footer/Footer"
 
 const App =()=>{
   return(
       <div>
         <Header />
         <Home />
+        <Footer/>
       </div>
   )
 }
---
+++ src/static/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css"
+const Footer =()=>{
+    return(
+        <div className="foot">
+            <hr/>
+            <h4>@2024 Raheem Muiz</h4>
+        </div>
+    )
+}
+export default Footer
